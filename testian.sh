301872999b7d563473b561fa8b1d6e5a71decf3f
ianmcculloch <ianmcculloch@users.noreply.github.com>
2016-02-20 11:32:07 +0100

    Update testian.sh
    added comments

--- testian.sh
+++ testian.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #try-catch block to update the system sources lists
 { # your 'try' block
+	# update the system sources & send notification
 	notify-send 'Aktualisiere Quellen...' --icon=dialog-information
-	apt-get update -qq &&
+	apt-get update -qq && 
 	notify-send 'Quellen aktualisiert.' --icon=dialog-information &&
+	# upgrade packages whithout confirmation & send notification
 	apt-get upgrade -y &&
 	notify-send 'Aktualisierungen angewandt' --icon=dialog-information &&
-	notify-send 'Leere Papierkorb des Systems...' --icon=user-trash &&
+	# autoremove & send notification
 	apt-get autoremove -y &&
-	notify-send 'Reinige den Arbeitsort des Systems...' --icon=user-desktop &&
+	notify-send 'Leere Papierkorb des Systems...' --icon=user-trash &&
+	# autoclean & send notification
 	apt-get autoclean -y
+	notify-send 'Reinige den Arbeitsort des Systems...' --icon=user-desktop &&
 } || { # your 'catch' block
+	#s send error notification
 	notify-send 'Aktualisierung fehlgeschlagen.' --icon=dialog-error
 } # finally: this will always happen
