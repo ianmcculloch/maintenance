0ee7b3555d7778ff6b6cc47f8628daa9ff6698eb
ianmcculloch <ianmcculloch@users.noreply.github.com>
2016-02-20 12:05:12 +0100

    Added files via upload

--- testian.sh
+++ testian.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+# script to upgrade all packages to the most recent version and empty the trash bin
 #try-catch block to update the system sources lists
 { # your 'try' block
-	# update the system sources & send notification
 	notify-send 'Aktualisiere Quellen...' --icon=dialog-information
-	apt-get update -qq && 
+	apt-get update -qq &&
 	notify-send 'Quellen aktualisiert.' --icon=dialog-information &&
-	# upgrade packages whithout confirmation & send notification
 	apt-get upgrade -y &&
 	notify-send 'Aktualisierungen angewandt' --icon=dialog-information &&
-	# autoremove & send notification
+	notify-send 'Leere alte Dateien des Systems...' --icon=user-trash &&
 	apt-get autoremove -y &&
-	notify-send 'Leere Papierkorb des Systems...' --icon=user-trash &&
-	# autoclean & send notification
-	apt-get autoclean -y
 	notify-send 'Reinige den Arbeitsort des Systems...' --icon=user-desktop &&
+	apt-get autoclean -y
 } || { # your 'catch' block
-	#s send error notification
 	notify-send 'Aktualisierung fehlgeschlagen.' --icon=dialog-error
 } # finally: this will always happen
+
+{ # try to empty trash
+	notify-send 'Leere Papierkorb...' --icon=user-trash &&
+	trash-empty
+	notify-send 'Papierkorb geleert.' --icon=user-trash
+} || { # catch any errors
+	notify-send 'Leerung fehlgeschlagen!' --icon=dialog-error
+}
